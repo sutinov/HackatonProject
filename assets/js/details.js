d1bea5401cec492c013c63db530dbfec4532953c
Blagoj Sutinov <bsutinov@gmail.com>
2021-01-17 17:21:25 +0100

    Add files via upload

---
+++ assets/js/details.js
@@ -0,0 +1,143 @@
+const userParams = new URLSearchParams(window.location.search);
+const contact = userParams.get("userId");
+const containerdiv = document.getElementById("container");
+let contactDetail = [];
+let user = [];
+let delUser = {
+  contactId: `${contact}`,
+};
+let newUser = [];
+let updateData = [];
+
+function contactDetails() {
+  let httpRequest = new XMLHttpRequest();
+  console.log(contact);
+  let userUrl = `http://api.contact.techup.me/contact/single?id=${contact}`;
+
+  httpRequest.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      contactDetail = JSON.parse(this.response);
+      console.log(contactDetail);
+      generateContactDetails();
+    }
+  };
+
+  httpRequest.open("GET", userUrl);
+  httpRequest.send();
+}
+
+function generateContactDetails() {
+  containerdiv.innerHTML = "";
+  let detailCard = document.createElement("div");
+  detailCard.className = "card";
+  detailCard.innerHTML += ` <img class="card-img-top profile-img rounded-circle" src="${contactDetail.photo}" alt="Card image cap" />
+        <div class="card-body">
+          <h5 class="card-title">${contactDetail.firstName} ${contactDetail.lastName}</h5>
+          <p class="card-text">
+            Mobile Phone: ${contactDetail.mobilePhone}
+          </p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">email: ${contactDetail.email}</li>
+          <li class="list-group-item">gender: ${contactDetail.gender}</li>
+          <li class="list-group-item">Category: ${contactDetail.category}</li>
+        </ul>
+        <div class="card-body">
+          
+        </div>`;
+  containerdiv.append(detailCard);
+}
+
+function deleteContact() {
+  let httpRequest = new XMLHttpRequest();
+  let delUrl = `http://api.contact.techup.me/contact/delete?id=${contact}`;
+
+  httpRequest.onreadystatechange = function () {
+    console.log("4");
+    if (this.readyState === 4 && this.status === 200) {
+      delUser = JSON.parse(this.response).items;
+      console.log(delUser);
+      goHome();
+    }
+  };
+
+  httpRequest.open("POST", delUrl);
+  httpRequest.setRequestHeader("Content-Type", "application/json");
+  httpRequest.send(JSON.stringify(delUser));
+}
+
+function goHome() {
+  window.location.href = "./index.html";
+}
+
+// function editDetails() {}
+
+function createContact() {
+  let httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      newUser = JSON.parse(this.response);
+      console.log("New User", newUser);
+    }
+  };
+
+  httpRequest.open("POST", "http://api.contact.techup.me/contact/create");
+  httpRequest.setRequestHeader("Content-Type", "application/json");
+  let newUser = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    mobilePhone: document.getElementById("mobilePhone").value,
+    email: document.getElementById("email").value,
+    userId: "32sgEH9kVQ2T30UXYN09qA==",
+  };
+  httpRequest.send(JSON.stringify(newUser));
+}
+
+// function editContact() {
+//   let httpRequest = new XMLHttpRequest();
+//   console.log("1");
+//   httpRequest.onreadystatechange = function () {
+//     if (this.readyState === 4 && this.status === 200) {
+//       updateUser = JSON.parse(this.response);
+//       console.log("New User", updateUser);
+//     }
+//     console.log("2");
+//   };
+
+//   httpRequest.open("POST", "http://api.contact.techup.me/contact/update");
+//   httpRequest.setRequestHeader("Content-Type", "application/json");
+//   let updateUser = {
+//     firstName: document.getElementById("firstName").value,
+//     lastName: document.getElementById("lastName").value,
+//     mobilePhone: document.getElementById("mobilePhone").value,
+//     email: document.getElementById("email").value,
+//     userId: `${contact}`,
+//   };
+//   httpRequest.send(JSON.stringify(updateUser));
+// }
+
+function updateContact() {
+  let httpRequest = new XMLHttpRequest();
+
+  httpRequest.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      console.log(updateData);
+      updateData = JSON.parse(this.response);
+    }
+  };
+
+  httpRequest.open("POST", "http://api.contact.techup.me/contact/update", true);
+
+  httpRequest.setRequestHeader("Content-Type", "application/json");
+
+  let updateData = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    mobilePhone: document.getElementById("mobilePhone").value,
+    email: document.getElementById("email").value,
+    category: document.getElementById("category").value,
+    userId: "32sgEH9kVQ2T30UXYN09qA==",
+  };
+
+  httpRequest.send(JSON.stringify(updateData));
+}
---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+let contacts = {
+  userId: "32sgEH9kVQ2T30UXYN09qA==",
+  pageNumber: 4,
+  pageSize: 20,
+  sortColumn: "firstName",
+  sortDirection: "asc",
+};
+let user = [];
+
+const userCard = document.getElementById("container");
+
+function getContacts() {
+  let httpRequest = new XMLHttpRequest();
+
+  httpRequest.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      contacts = JSON.parse(this.response).items;
+
+      console.log("CONTACTS", contacts);
+      generateCards();
+    }
+  };
+
+  httpRequest.open("POST", "http://api.contact.techup.me/contact/list");
+  httpRequest.setRequestHeader("Content-Type", "application/json");
+  httpRequest.send(JSON.stringify(contacts));
+}
+
+function generateCards() {
+  userCard.innerHTML = "";
+  for (let i = 0; i < contacts.length; i++) {
+    contact = contacts[i];
+    let contactCard = document.createElement("div");
+    contactCard.className = "card";
+    contactCard.innerHTML += `
+        <div class="photo" style="background-image: url('${contact.photo}')"></div>
+        <div class="banner"></div>
+        <ul>
+         <a href="./contact_details.html?userId=${contact.contactId}"> <li><b>${contact.firstName} ${contact.lastName}</b></li></a>
+          <li>phone: ${contact.mobilePhone}</li>
+          <li>email: ${contact.email}</li>
+        </ul>
+        <a href="./contact_details.html?userId=${contact.contactId}"><button class="contact" id="main-button">Open contact</button></a>
+      </div>`;
+    userCard.append(contactCard);
+  }
+}
+
+function openContact() {
+  window.location.href = `./contact_details.html?userId=${contact.contactId}`;
+}
